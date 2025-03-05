013a08ee82927d7d53873e5aa572653bfed9f4ef
fj tria <fjtria.dev@gmail.com>
2025-03-04 23:48:29 -0800

    dex entry style

--- src/components/PokedexEntry/PokedexEntry.jsx
+++ src/components/PokedexEntry/PokedexEntry.jsx
@@ -9,6 +9,12 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPokemonDetails } from '../../services/PokeAPI';
 
+// converts height to meters
+function convertHeight(height)  { return (height / 10); }
+
+// converts weight to kilograms
+function convertWeight(weight) { return (weight / 10); }
+
 export default function PokedexEntry() {
 	const { id } = useParams();		// get pokemon id from url path
 	const [details, setDetails] = useState(null);			// core pokemon data
@@ -23,42 +29,69 @@ export default function PokedexEntry() {
 	}, [id]);
 
 	return (
-		<div id="pokedex-entry">
+		<>
 			{details && (
-				<>
-					<img 
-						className='pokemon-artwork'
-						src={details.sprites.other['official-artwork'].front_default} 
-						alt={details.name}
-					></img>
-					<div>
-						<div className='pokemon-name'>
-							<h2>#{speciesData?.id}</h2>
-							<h2>{details.name}</h2>
+				<div id="pokedex-entry">
+					<div className='data-section'>
+						<div className='data-image'>
+							<img 
+								className='pokemon-artwork'
+								src={details.sprites.other['official-artwork'].front_default} 
+								alt={details.name}
+							></img>
 						</div>
+						<div className='data-text'>
+							<h2 className='pokedex-number'>#{speciesData?.id}</h2>
+							<h1>{details.name}</h1>
 
-						<div className='pokemon-types'>
-							Type:
-							{details.types.map((type) => (
-							<p
-								className={`type-card ${type.type.name}-type`}
-								key={type.type.name}
-							>
-								{type.type.name}
-							</p>
-							))}
+							<h2>Type:</h2>
+							<div className='pokemon-types'>
+								{details.types.map((type) => (
+									<p
+										className={`type-card ${type.type.name}-type`}
+										key={type.type.name}
+									>
+										{type.type.name}
+									</p>
+								))}
+							</div>
+							
+							<h2>Height:</h2>
+							<p>{convertHeight(details.height)} m</p>
+
+							<h2>Weight:</h2>
+							<p>{convertWeight(details.weight)} kg</p>
+
+							<h2>Abilities:</h2>
+							<div className='pokemon-abilities'>
+								{details.abilities.map((ability) => (
+									<p key={ability.ability.name}>
+										{ability.ability.name}
+									</p>
+								))}
+							</div>
 						</div>
+					</div>
 
-						<div className="pokemon-stats">
+					<div className="pokemon-stats">
+						<h2>Stats:</h2>
+						<div className='stat-names'>
 							{details.stats.map((stat) => (
 								<p key={stat.stat.name}>
-									{stat.stat.name} – {stat.base_stat}
+									{stat.stat.name}
+								</p>
+							))}
+						</div>
+						<div className='stat-values'>
+							{details.stats.map((stat) => (
+								<p key={stat.stat.name}>
+									{stat.base_stat}
 								</p>
 							))}
 						</div>
 					</div>
-				</>
+				</div>
 			)}
-		</div>  
+		</>
 	);
 }
