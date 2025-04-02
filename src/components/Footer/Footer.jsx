a2107991620b04cb4614a64ee8ed0fef03e55086
fj tria <fjtria.dev@gmail.com>
2025-04-02 16:51:07 -0700

    version no

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ import "./Footer.css"
 export default function Navbar() {
     return (
         <footer>
-            <p>v1.0.0 • Produced 2025 by <a href='https://fjtria.dev/' rel='noopener noreferrer' target='_blank'>@fjtria.dev</a></p>
+            <p>v1.1.0 • Produced 2025 by <a href='https://fjtria.dev/' rel='noopener noreferrer' target='_blank'>@fjtria.dev</a></p>
             <p>Pokémon and Respective Names are Trademark of Nintendo 1996-2025</p>
         </footer>
     );
